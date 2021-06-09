a22b2215d3ee794e3e77fbed1e923572ab34337e
Kelli Landry <landry.kel@gmail.com>
2021-06-09 15:37:41 -0500

    memo & callback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
 
-import './App.css';
+import React, { useState, useCallback, useMemo } from 'react'
 
-function App() {
+import './App.css'
+import DemoList from './components/Demo/Demo'
+import Button from './components/UI/Button/Button'
+
+function App () {
+  const [listTitle, setListTitle] = useState('My List')
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, [])
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], [])
+
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
+    <div className='app'>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import classes from './Button.module.css';
+import classes from './Button.module.css'
 
 const Button = (props) => {
+  console.log('Button RUNNING')
   return (
     <button
       type={props.type || 'button'}
@@ -12,7 +13,7 @@ const Button = (props) => {
     >
       {props.children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default React.memo(Button)
